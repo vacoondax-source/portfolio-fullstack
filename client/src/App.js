fa6519b04e042db9c8dd46cbf0fb46f0fe78b13d
Abdull warfaa <vacoondax@gmail.com>
2026-03-28 09:07:26 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Projects from "./pages/Projects";
-import Contact from "./pages/Contact";
-import Dashboard from "./pages/Dashboard";
-
 function App() {
   return (
-    <Router>
-      <Navbar />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
+    <div>
+      <h1>MY APP IS WORKING 🚀</h1>
+    </div>
   );
 }
 
